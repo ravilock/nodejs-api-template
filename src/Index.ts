b2182ecb84fb9b7aa09b866f5d730845bda574a2
ravilock <ravi.me.professional@hotmail.com>
2022-03-26 00:49:05 -0300

    feat: add server starter functions

---
+++ src/Index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { Server } from './Server';
+import { App } from './App';
+import { KnexInstance } from './Database/knex';
+
+async function start() {
+    await testDatabase();
+    const server = new Server(new App(), KnexInstance);
+    server.startServer();
+}
+
+async function testDatabase() {
+    try {
+        await KnexInstance.raw('SELECT now()');
+    } catch (error) {
+        throw new Error(`Database connection not set up properly: ${error}`);
+    }
+}
+
+start().then();
